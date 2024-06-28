2309d5bb5de50a819a2fb9064de4fb0c6e64c061
Schmurtz <schmurtz@gmail.com>
2024-06-28 14:20:15 +0200

    Make PSX Analog Detector.sh 12x faster

--- SystemTools/Apps/SystemTools/Menu/EMULATORS/PSX Analog Detector.sh
+++ SystemTools/Apps/SystemTools/Menu/EMULATORS/PSX Analog Detector.sh
@@ -4,13 +4,15 @@ echo 1416000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
 RA_DIR=/mnt/SDCARD/RetroArch
 cd $RA_DIR
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui --scan=/mnt/SDCARD/Roms/PS
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui --appendconfig  "$RA_DIR/.retroarch/database/rdb/Sony - PlayStation - analog/Sony - PlayStation - analog.cfg"  --scan=/mnt/SDCARD/Roms/PS
+
+
 analog_count="/tmp/analog_count"
 echo 0 >"$analog_count"
 
-mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/PCSX-ReARMed"
-mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/DuckStation"
-mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
+mkdir -p "$RA_DIR/.retroarch/config/remaps/PCSX-ReARMed"
+mkdir -p "$RA_DIR/.retroarch/config/remaps/DuckStation"
+mkdir -p "$RA_DIR/.retroarch/config/remaps/SwanStation"
 
 /mnt/SDCARD/System/bin/sdl2imgshow \
   -i "/usr/trimui/res/skin/bg.png" \
@@ -20,19 +22,11 @@ mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
   -t "Detecting Dual Shock compatible games..." &
 
 # Traverse the "Sony - PlayStation.lpl" file with jq
-/mnt/SDCARD/System/bin/jq -r '.items[] | .crc32, .label, .path' "/mnt/SDCARD/RetroArch/.retroarch/playlists/Sony - PlayStation.lpl" |
+/mnt/SDCARD/System/bin/jq -r '.items[] | .crc32, .label, .path' "$RA_DIR/.retroarch/playlists/Sony - PlayStation - analog.lpl" |
   while IFS= read -r crc32; do
     IFS= read -r label
     IFS= read -r path
 
-    # Extract CRC32
-    crc32=$(echo "$crc32" | awk -F '|' '{print $1}')
-
-    # Search for CRC32 in psx_analog_gamelist.dat
-    found_path=$(grep -E "$crc32" "/mnt/SDCARD/System/usr/trimui/scripts/psx_analog_gamelist.dat")
-
-    # If path is found, create the file with .rmp extension
-    if [ -n "$found_path" ]; then
       # Create file path with .rmp extension
       count=$(cat "$analog_count")
       count=$((count + 1))
@@ -40,7 +34,7 @@ mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
       filename=$(basename "$path")
       filename="${filename%.*}.rmp"
       # ========= PCSX-ReARMed
-      filepath="/mnt/SDCARD/RetroArch/.retroarch/config/remaps/PCSX-ReARMed/$filename"
+      filepath="$RA_DIR/.retroarch/config/remaps/PCSX-ReARMed/$filename"
       echo "Applying Dual Shock to $filename for PCSX-ReARMed"
       if [ -e "$filepath" ]; then
         configPatchFile="/tmp/$filename"
@@ -52,7 +46,7 @@ mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
         echo 'input_libretro_device_p1 = "517"' >"$filepath"
       fi
       # ========= Duckstation
-      filepath="/mnt/SDCARD/RetroArch/.retroarch/config/remaps/DuckStation/$filename"
+      filepath="$RA_DIR/.retroarch/config/remaps/DuckStation/$filename"
       echo "Applying Dual Shock to $filename for PCSX-ReARMed"
       if [ -e "$filepath" ]; then
         configPatchFile="/tmp/$filename"
@@ -64,7 +58,7 @@ mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
         echo 'input_libretro_device_p1 = "5"' >"$filepath"
       fi
       # ========= Swantation
-      filepath="/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation/$filename"
+      filepath="$RA_DIR/.retroarch/config/remaps/SwanStation/$filename"
       echo "Applying Dual Shock to $filename for SwanStation"
       if [ -e "$filepath" ]; then
         configPatchFile="/tmp/$filename"
@@ -75,8 +69,10 @@ mkdir -p "/mnt/SDCARD/RetroArch/.retroarch/config/remaps/SwanStation"
         # Write content to the file
         echo 'input_libretro_device_p1 = "517"' >"$filepath"
       fi
-    fi
   done
+  
+rm "$RA_DIR/.retroarch/playlists/Sony - PlayStation - analog.lpl"
+  
 sync
 
 echo "Number of successful path finds: $(cat "$analog_count")"
